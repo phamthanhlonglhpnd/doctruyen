a7568269a42151b9235b89c0291c6d3b706748e8
Hoang Quang Huy <hqhuy@Hoangs-MacBook-Pro-2.local>
2022-08-30 10:38:20 +0700

    log useHosting

--- src/hooks/useHosting.ts
+++ src/hooks/useHosting.ts
@@ -3,6 +3,8 @@ import { useSupabaseSingleQuery } from "@/utils/supabase";
 import { supabaseClient as supabase } from "@supabase/auth-helpers-nextjs";
 
 const useHosting = (hostingId: string) => {
+  console.log(hostingId);
+  
   return useSupabaseSingleQuery(["hosting", hostingId], () =>
     supabase.from<Hosting>("kaguya_hostings").select("*").eq("id", hostingId)
   );
